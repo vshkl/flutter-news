44073a7ed2ee01b7ae6ccc4e8bdac9b3b25800d9
Pavel Vashkel <p.vashkel@datarockets.com>
2022-11-10 22:29:33 +0100

    Reformat news.dart

--- lib/model/News.dart
+++ lib/model/News.dart
@@ -5,11 +5,12 @@ part 'news.g.dart';
 
 @freezed
 class News with _$News {
-  const factory News(
-      {required String title,
-      required String author,
-      required int points,
-      @JsonKey(name: 'num_comments') required int comments}) = _News;
+  const factory News({
+    required String title,
+    required String author,
+    required int points,
+    @JsonKey(name: 'num_comments') required int comments,
+  }) = _News;
 
   factory News.fromJson(Map<String, dynamic> json) => _$NewsFromJson(json);
 }
